e4476f8f984a0aa93f7e63b173d48b093ed7804d
carlosberrio <carlosberriomon@yahoo.com>
2022-03-24 10:23:53 -0700

    prueba checkers

--- print_numbers.c
+++ print_numbers.c
@@ -6,7 +6,7 @@
  *
  * Return: length of the string without null byte
  */
-int print_decimal(va_list d)
+/**int print_decimal(va_list d)
 {
 	int i = 0;
 	char buffer[1024];
@@ -18,7 +18,7 @@ int print_decimal(va_list d)
 	}
 	return (_strlen(buffer));
 }
-
+*/
 /**
  * print_integer - print unsigned integer
  * @integer: unsigned integer to be printed
@@ -37,3 +37,47 @@ int print_integer(va_list integer)
 	}
 	return (_strlen(buffer));
 }
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+		n1 = n;
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
+}
+int print_decimal(va_list args)
+{
+	int n = va_arg(args, int);
+	int sum = count_digit(n);
+
+	if (n <= 0)
+	sum++;
+		print_number(n);
+	return (sum);
+}
+
