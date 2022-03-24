8d5432af09b0e90a53a213023549e1e0d7989d03
Santiago Bedoya <santiagobedoa@gmail.com>
2022-03-24 10:39:02 -0500

    Thu Mar 24 10:39:02 -05 2022

--- print_numbers.c
+++ print_numbers.c
@@ -11,11 +11,6 @@ int print_decimal(va_list d)
 	int i = 0;
 	char buffer[1024];
 
-	if (va_arg(d, int) == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
 	int_to_arg(va_arg(d, int), buffer, 10);
 	for (; buffer[i] != '\0'; i++)
 	{
@@ -35,16 +30,10 @@ int print_integer(va_list integer)
 	int i = 0;
 	char buffer[1024];
 
-	if (va_arg(integer, int) == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
 	int_to_arg(va_arg(integer, int), buffer, 10);
 	for (; buffer[i] != '\0'; i++)
 	{
 		_putchar(buffer[i]);
 	}
-	/* return buffer - NULL byte */
 	return (_strlen(buffer));
 }
